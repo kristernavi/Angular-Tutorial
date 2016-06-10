14d594404106268d0e4df03a3d1e7fe710faa098
kristernavi <kristernavi@gmail.com>
2016-06-10 20:14:23 +0800

    Added Filter example

--- main.js
+++ main.js
@@ -1,4 +1,34 @@
-angular.module("myapp", [])
-.controller('FirstCtrl',function($scope) {
+var myapp = angular.module("myapp", []);
+ 
+ myapp.controller('FirstCtrl',function($scope) {
     $scope.data= {message:"hello"};
   });
+  
+ myapp.factory('Avengers',function()
+ {
+	 var Avengers = {};
+	 Avengers.cast = [
+	 {
+		 name: "Clark Gregg",
+		 character: "Agent Phil Coulson"
+	 },
+	 {
+		 name: "Stellan Skarsgard",
+		 character: "Selvig"
+	 },
+	 {
+		 name: "Samuel L Jackson",
+		 character: "Pepper Potts"
+	 }
+	 ];
+	 return Avengers;
+	 
+ } )
+ 
+ 
+
+  myapp.controller('AvengersCtrl',function($scope,Avengers) {
+     $scope.avengers = Avengers;
+  });
+ 
+ 
