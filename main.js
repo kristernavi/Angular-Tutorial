9b263f1d34c4fc3266947feaaca8b4eabff41380
kristernavi <kristernavi@gmail.com>
2016-06-13 10:22:54 +0800

    added direcive example

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 var myapp = angular.module("myapp", []);
- 
+
+var myapp2 = angular.module("superhero", [])
+
+myapp2.directive("superman",function(){
+		return {
+			restrict: "E",
+			template: "<div>Im here to save the day</div>"
+		}
+	}
+
+)
  myapp.controller('FirstCtrl',function($scope) {
     $scope.data= {message:"hello"};
   });
